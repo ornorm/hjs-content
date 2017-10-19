0d6f3f3d94de03c5f814a29dbcf921976dcf009c
biendo <aime@awelty.com>
2017-10-19 11:31:02 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# HJS-CONTENT
+> Content management library classes.
+
+Common classes like: Adapter, ArrayAdapter, ContentObservable, ContentObserver, LoaderManager etc...
+
+## Installation
+
+Node:
+
+```sh
+npm install hjs-content --save
+```
+
+## Meta
+
+Aime – abiendo@gmail.com
+
+Distributed under the MIT license. See ``LICENSE`` for more information.
---
+++ index.js
@@ -0,0 +1,51 @@
+/** @babel */
+import {
+    Adapter,
+    BaseAdapter,
+    ArrayAdapter,
+    FixedViewInfo,
+    HeaderViewListAdapter} from './lib/adapter';
+import {
+    ContentObservable,
+    ContentObserver,
+    DataSetObservable,
+    DataSetObserver} from './lib/dataset';
+import {
+    NotificationRunnable,
+    Transport,
+    ForceLoadContentObserver,
+    LoaderListener,
+    Loader,
+    LoadTask,
+    AsyncTaskLoader,
+    LoaderCallbacks,
+    LoaderManager,
+    LoaderManagerHost,
+    LoaderInfo,
+    LoaderManagerImpl} from './lib/loader';
+
+export {
+    Adapter,
+    BaseAdapter,
+    ArrayAdapter,
+    FixedViewInfo,
+    HeaderViewListAdapter,
+
+    ContentObservable,
+    ContentObserver,
+    DataSetObservable,
+    DataSetObserver,
+
+    NotificationRunnable,
+    Transport,
+    ForceLoadContentObserver,
+    LoaderListener,
+    Loader,
+    LoadTask,
+    AsyncTaskLoader,
+    LoaderCallbacks,
+    LoaderManager,
+    LoaderManagerHost,
+    LoaderInfo,
+    LoaderManagerImpl
+}
